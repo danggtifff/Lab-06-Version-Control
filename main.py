fd2c07a055f782928d74b746a0217a7ef9d99b89
danggtifff <63815352+danggtifff@users.noreply.github.com>
2023-10-31 13:01:10 -0400

    added option 3

--- main.py
+++ main.py
@@ -21,3 +21,5 @@ if __name__ == '__main__':
             print("Your password has been encoded and stored!\n")
         elif option == 2:
             print(f"The encoded password is {password}, and the original password is {decode.decode(password)}")
+        elif option == 3:
+            exit()
