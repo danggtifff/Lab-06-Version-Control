74ce14831cda88ebb6ba6f36556a0f353bb1467d
danggtifff <63815352+danggtifff@users.noreply.github.com>
2023-10-31 12:58:19 -0400

    edited main.py need decode.py

---
+++ main.py
@@ -0,0 +1,20 @@
+#This is my main.py
+
+def menu():
+    print("Menu"
+          "-------------"
+          "1. Encode"
+          "2. Decode"
+          "3. Quit\n")
+
+if __name__ == '__main__':
+    option = -1
+    while True:
+        menu()
+        option = input("Please enter an option: ")
+        if option == 1:
+            password = input("Please enter your password to encode: ")
+            password = encode.encode(password)
+            print("Your password has been encoded and stored!\n")
+        elif option == 2:
+            print(f"The encoded password is {password}, and the original password is {decode.decode(password)}")
